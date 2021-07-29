f7ba4594d2120338492fc7d86df4ed77688b5cf1
fl97 <50992060+fl97@users.noreply.github.com>
2021-07-29 16:24:02 +0200

    slight changes

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ document.getElementById('cashbox-tip').innerText = "$0.00"
 document.getElementById('cashbox-total').innerText = "$0.00"
 document.getElementById('people-input').value = 1
 
+let header = document.getElementsByClassName('btn-container')
+let buttons = document.getElementsByClassName('tip-amount-btn')
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+
+        var current = document.getElementsByClassName("active")
+
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "")
+        }
+
+        this.className += " active"   
+
+    })
+}
+
+console.log(document.querySelector('.active'))
+
 function calculateTip() {
     let stringBill = document.getElementById('bill-input').value
     let numberBill = parseFloat(stringBill)
